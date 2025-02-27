5bf9799967d8db25ff7a888c4556163f36032ed2
zhoucheng <zhoucheng133@outlook.com>
2025-02-27 18:52:41 +0800

    添加gin

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
-import "index_page_core/utils"
+import (
+	"index_page_core/routers"
+	"index_page_core/utils"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	utils.InitSql()
-	
+	r := gin.New()
+
+	r.GET("/api/list", routers.List)
+
+	r.Run(":8080")
 }
