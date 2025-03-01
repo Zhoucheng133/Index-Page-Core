e1838b263411a23080606ede995478742672a3d2
zhoucheng <zhoucheng133@outlook.com>
2025-03-01 19:16:09 +0800

    添加初始化用户表

--- utils/init_sql.go
+++ utils/init_sql.go
@@ -14,14 +14,23 @@ func InitSql() {
 		return
 	}
 	defer db.Close()
-	createTable := `CREATE TABLE IF NOT EXISTS pages (
+	createPage := `CREATE TABLE IF NOT EXISTS pages (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		name TEXT,
 		port TEXT,
 		webui INTEGER,
 		tip TEXT
 	);`
-	_, err = db.Exec(createTable)
+	_, err = db.Exec(createPage)
+	if err != nil {
+		log.Fatal(err)
+	}
+	createUser := `CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT,
+		password TEXT,
+	);`
+	_, err = db.Exec(createUser)
 	if err != nil {
 		log.Fatal(err)
 	}
