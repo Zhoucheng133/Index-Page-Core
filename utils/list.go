5a529d9950a0ad732ab47cd6c438d1fb67e7843b
zhoucheng <zhoucheng133@outlook.com>
2025-03-22 20:44:55 +0800

    完成保存文件

--- utils/list.go
+++ utils/list.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jaevor/go-nanoid"
@@ -14,7 +15,7 @@ type Page struct {
 	ICON  string `json:"icon" binding:"omitempty"`
 	Name  string `json:"name" binding:"required"`
 	Port  string `json:"port" binding:"required"`
-	WebUI int    `json:"webui" binding:"omitempty"`
+	WebUI string `json:"webui" binding:"required"`
 	Tip   string `json:"tip" binding:"omitempty"`
 }
 
@@ -56,19 +57,55 @@ func List(c *gin.Context) {
 
 func AddItem(c *gin.Context) {
 	var newPage Page
-	if err := c.ShouldBindJSON(&newPage); err != nil {
-		c.JSON(200, gin.H{"ok": false, "msg": fmt.Sprint("请求数据格式不正确", err)})
+	// if err := c.ShouldBind(&newPage); err != nil {
+	// 	c.JSON(200, gin.H{"ok": false, "msg": fmt.Sprint("请求数据格式不正确", err)})
+	// 	return
+	// }
+
+	newPage.WebUI = c.PostForm("webui")
+	newPage.Name = c.PostForm("name")
+	newPage.Tip = c.PostForm("tip")
+	newPage.Port = c.PostForm("port")
+
+	if len(newPage.Name) == 0 || len(newPage.Port) == 0 {
+		c.JSON(200, gin.H{"ok": false, "msg": "请求数据格式不正确"})
 		return
 	}
+
 	id, _ := nanoid.Standard(21)
-	id_string := id()
+	idString := id()
+
+	var iconPath string
+	file, err := c.FormFile("icon")
+
+	if err == nil {
+		// 创建存储路径
+		saveDir := fmt.Sprintf("./db/%s", idString)
+		if err := os.MkdirAll(saveDir, os.ModePerm); err != nil {
+			c.JSON(200, gin.H{"ok": false, "msg": "创建目录失败", "error": err.Error()})
+			return
+		}
+
+		// 保存文件
+		filePath := fmt.Sprintf("%s/icon.png", saveDir)
+		if err := c.SaveUploadedFile(file, filePath); err != nil {
+			c.JSON(200, gin.H{"ok": false, "msg": "保存文件失败", "error": err.Error()})
+			return
+		}
+
+		iconPath = fmt.Sprintf("%s/icon.png", idString)
+	} else {
+		iconPath = ""
+	}
+
 	query := `INSERT INTO pages (id, icon, name, port, webui, tip) VALUES (?, ?, ?, ?, ?, ?)`
-	_, err := db.Exec(query, id_string, newPage.ICON, newPage.Name, newPage.Port, newPage.WebUI, newPage.Tip)
+	_, err = db.Exec(query, idString, iconPath, newPage.Name, newPage.Port, newPage.WebUI, newPage.Tip)
 	if err != nil {
 		c.JSON(200, gin.H{"ok": false, "msg": "插入数据失败", "error": err.Error()})
 		return
 	}
-	c.JSON(200, gin.H{"ok": true, "msg": "数据插入成功", "id": id_string})
+
+	c.JSON(200, gin.H{"ok": true, "msg": "数据插入成功", "id": idString})
 }
 
 func DeleteItem(c *gin.Context) {
