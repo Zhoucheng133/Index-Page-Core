c1b4d27f45644cc4767ce558c2db1143ac2babcc
zhoucheng <zhoucheng133@outlook.com>
2025-03-23 19:11:33 +0800

    使用图标url存储

--- utils/list.go
+++ utils/list.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jaevor/go-nanoid"
@@ -57,49 +56,15 @@ func List(c *gin.Context) {
 
 func AddItem(c *gin.Context) {
 	var newPage Page
-	// if err := c.ShouldBind(&newPage); err != nil {
-	// 	c.JSON(200, gin.H{"ok": false, "msg": fmt.Sprint("请求数据格式不正确", err)})
-	// 	return
-	// }
-
-	newPage.WebUI = c.PostForm("webui")
-	newPage.Name = c.PostForm("name")
-	newPage.Tip = c.PostForm("tip")
-	newPage.Port = c.PostForm("port")
-
-	if len(newPage.Name) == 0 || len(newPage.Port) == 0 {
+	if err := c.ShouldBindJSON(&newPage); err != nil {
 		c.JSON(200, gin.H{"ok": false, "msg": "请求数据格式不正确"})
 		return
 	}
 
 	id, _ := nanoid.Standard(21)
 	idString := id()
-
-	var iconPath string
-	file, err := c.FormFile("icon")
-
-	if err == nil {
-		// 创建存储路径
-		saveDir := fmt.Sprintf("./db/%s", idString)
-		if err := os.MkdirAll(saveDir, os.ModePerm); err != nil {
-			c.JSON(200, gin.H{"ok": false, "msg": "创建目录失败", "error": err.Error()})
-			return
-		}
-
-		// 保存文件
-		filePath := fmt.Sprintf("%s/icon.png", saveDir)
-		if err := c.SaveUploadedFile(file, filePath); err != nil {
-			c.JSON(200, gin.H{"ok": false, "msg": "保存文件失败", "error": err.Error()})
-			return
-		}
-
-		iconPath = fmt.Sprintf("%s/icon.png", idString)
-	} else {
-		iconPath = ""
-	}
-
-	query := `INSERT INTO pages (id, icon, name, port, webui, tip) VALUES (?, ?, ?, ?, ?, ?)`
-	_, err = db.Exec(query, idString, iconPath, newPage.Name, newPage.Port, newPage.WebUI, newPage.Tip)
+	query := `INSERT INTO pages (id, icon, name, port, webui, tip) VALUES(?, ?, ?, ?, ?, ?)`
+	_, err := db.Exec(query, idString, newPage.ICON, newPage.Name, newPage.Port, newPage.WebUI, newPage.Tip)
 	if err != nil {
 		c.JSON(200, gin.H{"ok": false, "msg": "插入数据失败", "error": err.Error()})
 		return
